137b06ea2c7b751821a5bb1e17ecc83676421754
Marcelo Sercheli <msercheli@gmail.com>
2021-06-07 16:18:28 -0400

    Add rez files

---
+++ package.py
@@ -0,0 +1,49 @@
+name = "qtbase"
+
+version = "5.15.2"
+
+description = \
+    """
+    Qt Base
+    """
+
+with scope("config") as c:
+    # Determine location to release: internal (int) vs external (ext)
+
+    # NOTE: Modify this variable to reflect the current package situation
+    release_as = "ext"
+
+    # The `c` variable here is actually rezconfig.py
+    # `release_packages_path` is a variable defined inside rezconfig.py
+
+    import os
+    if release_as == "int":
+        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_INT"]
+    elif release_as == "ext":
+        c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
+
+    #c.build_thread_count = "physical_cores"
+
+requires = [
+    "gcc-6.3",
+]
+
+private_build_requires = [
+    "cmake",
+    "llvm",
+    "python-2",
+]
+
+variants = [
+    ["platform-linux", "arch-x86_64", "os-centos-7"]
+]
+
+# If want to use Ninja, run the `rez-build -i --cmake-build-system "ninja"`
+# or `rez-release --cmake-build-system "ninja"`
+
+uuid = "repository.qtbase"
+
+def commands():
+    env.PATH.prepend("{root}/bin")
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.QT_LOCATION = "{root}"
